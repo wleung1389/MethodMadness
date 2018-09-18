019bfaecec2b15d0771270210e06b46f3d18800e
BT_1N3_20@bktech.local <BT_1N3_20@bktech.local>
2018-09-18 09:16:38 -0400

    Initial commit

---
+++ src/WLeung/TestFile.java
@@ -0,0 +1,9 @@
+package WLeung;
+
+import static Wleung.WleungLib.println;
+
+public class TestFile {
+    public static void  main(String[] args) {
+        println("I love a cup of java in the morning");
+    }
+}
---
+++ src/WLeung/WLeungLib.java
@@ -0,0 +1,7 @@
+package WLeung;
+
+public class WLeungLib {
+    public static void main(String str){
+        System.out.println(str);
+    }
+}
