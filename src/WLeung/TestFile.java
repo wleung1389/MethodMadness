ff5ffe2e02935fafe39fa89bc29d934b0a93ad48
Wai Hin Leung <wleung1389@bths.edu>
2018-09-20 09:41:12 -0400

    Commit

--- src/WLeung/TestFile.java
+++ src/WLeung/TestFile.java
@@ -1,9 +1,17 @@
 package WLeung;
 
-import static Wleung.WleungLib.println;
+/** 9/18/2018 By Wai Hin Leung
+ *
+ */
+import static WLeung.WLeungLib.*;
 
 public class TestFile {
-    public static void  main(String[] args) {
-        println("I love a cup of java in the morning");
+    public static void  main(String[] args)
+    {
+        println("I love a" + " cup of java in the morning");
+        System.out.println(isPalindrome("racecar"));
+        System.out.println(dateStr("08/01/2001"));
+        System.out.println(sumUpTo(10));
+        System.out.println(leastCommonMultiple(1,2,3));
     }
 }
--- src/WLeung/WLeungLib.java
+++ src/WLeung/WLeungLib.java
@@ -1,7 +1,48 @@
 package WLeung;
 
+/** 9/18/2018 By Wai Hin Leung
+ *
+ */
 public class WLeungLib {
-    public static void main(String str){
+    public static void println(String str){
         System.out.println(str);
+    }
+    public static boolean isPalindrome(String str)
+    {
+        String str1 = "";
+        str = str.toLowerCase();
+        for(int i = str.length(); i > 0; i--)
+        {
+            str1 += str.substring(i-1,i);
+        }
+        return str1.equals(str);
+    }
+    public static String dateStr(String str)
+    {
+        String[] array = new String[10];
+        array = str.split("/");
+        String str1 = array[1] + "-" + array[0] + "-" + array[2];
+        return str1;
+    }
+    public static int sumUpTo(int a)
+    {
+        int sum = 0;
+        for(int i = 0; i <= a; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }
+    public static int leastCommonMultiple(int a, int b , int c)
+    {
+        int LCM = 0;
+        for(int i = 2; i > a*b*c; i++)
+        {
+            if(i%a == 0 && i%b == 0 && i%c == 0)
+            {
+                LCM = i;
+            }
+        }
+        return LCM;
     }
 }
